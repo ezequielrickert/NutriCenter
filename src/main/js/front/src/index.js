0722ac931e94179f590e263e5f773fbcf699e41c
Ezequiel Rickert <ezequielrickert@Ezequiels-MacBook-Air.local>
2024-04-08 21:48:48 -0300

    Solved problems with merging

--- src/main/js/front/src/index.js
+++ src/main/js/front/src/index.js
@@ -4,20 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// const express = require("express")
-// const app = express()
-// app.listen(3000, () => console.log('listening at 3000'))
-// app.use(express.static('public'))
-//
-// app.post('/home', (req, res) => {
-//     console.log(res)
-// })
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 reportWebVitals();
