7b7c248b8f11e81fa5ac54df2ebdac7dee42ffc2
ezequielrickert <erickert@mail.austral.edu.ar>
2024-04-22 09:01:24 -0300

    Nutritionist Verification In this commit, if any user tries to access to a private nutritionist endpoint, it will be redirected to login.

--- src/main/js/front/src/view/nutritionist/edition/initialEditor.jsx
+++ src/main/js/front/src/view/nutritionist/edition/initialEditor.jsx
@@ -1,11 +1,39 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import NutritionistCreateRecipe from '../edition/recipeActions/nutritionistCreateRecipe';
 import NutritionistUpdateRecipe from '../edition/recipeActions/nutritionistUpdateRecipe';
 import NutritionistDeleteRecipe from '../edition/recipeActions/nutritionistDeleteRecipe';
 import Footer from "../../components/footer";
+import axios from "axios";
 
 const NutritionistRecipeEditor = () => {
     const [operation, setOperation] = useState('');
+    const [isValidUser, setIsValidUser] = useState(false);
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('username');
+    const [isPublic, setIsPublic] = useState(true);
+    const userRole = localStorage.getItem('role');
+
+    useEffect(() => {
+        const validateUser = async () => {
+            try {
+                const response = await axios.post("http://localhost:8080/validateUser", { username, token });
+                if (response.data === "User is valid" && userRole === "nutritionist") {
+                    setIsValidUser(true);
+                } else {
+                    window.location.href = '/universalLogin';
+                }
+            } catch (error) {
+                console.error("Error validating user", error);
+                window.location.href = '/universalLogin';
+            }
+        };
+
+        validateUser();
+    }, [token, username]);
+
+    if (!isValidUser) {
+        return null;
+    }
 
     const handleSelectChange = (event) => {
         setOperation(event.target.value);
